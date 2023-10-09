89ddc50f14faaf06aa00873c36bfc83224da8259
EA - Rafael Rodrigues De Almeida <rafael_almeida01@educadventista.org>
2023-10-09 15:02:55 -0300

    recebendo parametros na url

--- 2_EXPRESS/5_RECAP/index.js
+++ 2_EXPRESS/5_RECAP/index.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
-app.get()
+const caminho = path.join(__dirname, "templetes")
+
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhop}/index.html`)
+})
 
 app.listen(3000, () => {
     console.log("servidor rodando na porta 3000")
---
+++ 2_EXPRESS/6_parametro_url/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+
+const path = require("path")
+
+const caminho = path.join(__dirname, "templates")
+
+app.get("/usuarios/:id", (requisicao, resposta) =>{
+    const id = requisicao.params.id
+    console.log(` o ususario ${id} esta desaparecido`)
+    resposta.sendFile(`${caminho}/usuarios.html`)
+})
+
+app.get("/", (requisicao, resposta) =>{
+    resposta.sendFile(`${caminho}/index.html`)
+})
+
+app.listen(3000, () => {
+    console.log("servidor rodando na porta 3000")
+})
